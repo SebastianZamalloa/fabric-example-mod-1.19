331d00dc9c3d9137fc7cfaef0f95e5edddc6c12a
SebastianZamalloa <szamalloam@unsa.edu.pe>
2022-07-30 21:04:06 -0500

    Tutorial item

--- src/main/java/net/dogez/tutorialmod/TutorialMod.java
+++ src/main/java/net/dogez/tutorialmod/TutorialMod.java
@@ -1,14 +1,16 @@
 package net.dogez.tutorialmod;
 
+import net.dogez.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
